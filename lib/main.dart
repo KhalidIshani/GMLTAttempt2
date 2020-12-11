0d4125cccc76e0535ac70dc05bfe55bab61f1a1a
KhalidIshani <Khalidishani@gmail.com>
2020-12-11 10:25:32 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
+//import necessary packages
 import 'dart:async';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'json.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:convert';
 import 'package:path_provider/path_provider.dart';
-
+//initialize new maze
 maze maze1= new maze();
 Color color1 = Colors.grey;
 int lastMove = 0; //records last CORRECT move of user
-bool lastMoveIncorrect = true; //was last move of user correct
+bool lastMoveIncorrect = true; //true if user's last move was correct, false otherwise
 var icon = Icons.check;
 var image="assets/greencheck.jpg";
-Stopwatch clock = new Stopwatch();
+Stopwatch clock = new Stopwatch(); //initialize new stopwatch that will be used to record times of user's moves
 List moves = [];
 List correctMoves = [];
 List<dynamic> times=[];
 List errors = [];
 //temporary test path - going to change to make dynamically generated
 List<dynamic> path=[0,10,20,30,40,50,60,70,80,90,91,92,93,94,95,96,97,98,99];
-bool timeOut = false;
+bool timeOut = false; //when true, user is prohibited from entering new moves (so as not to overwhelm game)
 var dateTime = DateTime.now();
 int attemptNum = 1;
 int consecErrors = 0;
@@ -29,25 +26,11 @@ int recentMove; //records last move of user regardless of corectness
 
 void main() {
   runApp(MyApp());
-  maze1.button_grid[0].onPath=1;
-  maze1.button_grid[10].onPath=1;
-  maze1.button_grid[20].onPath=1;
-  maze1.button_grid[30].onPath=1;
-  maze1.button_grid[40].onPath=1;
-  maze1.button_grid[50].onPath=1;
-  maze1.button_grid[60].onPath=1;
-  maze1.button_grid[70].onPath=1;
-  maze1.button_grid[80].onPath=1;
-  maze1.button_grid[90].onPath=1;
-  maze1.button_grid[91].onPath=1;
-  maze1.button_grid[92].onPath=1;
-  maze1.button_grid[93].onPath=1;
-  maze1.button_grid[94].onPath=1;
-  maze1.button_grid[95].onPath=1;
-  maze1.button_grid[96].onPath=1;
-  maze1.button_grid[97].onPath=1;
-  maze1.button_grid[98].onPath=1;
-  maze1.button_grid[99].onPath=1;
+  //manually fill in maze
+  for(var i=0; i<19; i++)
+    {
+      maze1.button_grid[path[i]].onPath=1;
+    }
 }
 
 class MyApp extends StatelessWidget {
@@ -66,9 +49,11 @@ class MyApp extends StatelessWidget {
 
 class gameButton extends StatefulWidget {
   int id;
+  //id represents id of button (top left button is 0 bottom right button is 99)
   Color color;
   bool displayImage;
   int onPath=0;
+  //1 if square is on the path, 0 otherwise
   bool testOnPath() {
     if(this.onPath==1)
       {
@@ -85,6 +70,7 @@ class gameButton extends StatefulWidget {
     return (((this.id - lastMove).abs() == 1) ^
     ((this.id - lastMove).abs() == 10));
   }
+  //tests if move is legal
   bool moveCheck()
   {
     if(lastMoveIncorrect)
@@ -113,14 +99,17 @@ class gameButton extends StatefulWidget {
           }
       }
   }
+  //function above returns true if move user makes is correct (both a legal move and a square that is on the path) and false otherwise
   @override
   gameButton(this.id, this.color, this.displayImage);
+  //initialize state
   gameButtonState createState() => gameButtonState(color);
 
 }
 
 class gameButtonState extends State<gameButton> {
 
+  //code below used to dynamically generate json files of the user's moves
   File jsonFile;
   Directory dir;
   String fileName = "Maze task attempt $attemptNum - $dateTime";
@@ -144,26 +133,24 @@ class gameButtonState extends State<gameButton> {
     super.dispose();
   }
 
+  //if file doesn't exist need to create new json file
   void createFile(Map<String, dynamic> content, Directory dir,
       String fileName) {
-    print("Creating file!");
     File file = new File(dir.path + "/" + fileName);
     file.createSync();
     fileExists = true;
     file.writeAsStringSync(json.encode(content));
   }
 
+  //if file w/t same name exists, need to edit file instead of creating new one
   void writeToFile(String key, dynamic value) {
-    print("Writing to file!");
     Map<String, dynamic> content = {key: value};
     if (fileExists) {
-      print("File exists");
       Map<String, dynamic> jsonFileContent = json.decode(
           jsonFile.readAsStringSync());
       jsonFileContent.addAll(content);
       jsonFile.writeAsStringSync(json.encode(jsonFileContent));
     } else {
-      print("File does not exist!");
       createFile(content, dir, fileName);
     }
     this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));
@@ -172,8 +159,10 @@ class gameButtonState extends State<gameButton> {
 
   Color color;
 
+  //initialize color state of buttons
   gameButtonState(this.color);
 
+  //after user clicks next square the previous square should turn back to grey
   void turnGrey() {
     if (moves.length >= 2) {
       if (widget.id == moves[moves.length - 2]) {
@@ -187,6 +176,7 @@ class gameButtonState extends State<gameButton> {
     }
   }
 
+  //if user makes 3 incorrect moves in a row then game shows the user the next correct move by turning it green
   void mercyRule() {
     if (consecErrors >= 3) {
       timeOut = true;
@@ -200,7 +190,7 @@ class gameButtonState extends State<gameButton> {
       null;
     }
   }
-
+//once user reaches end of maze, game should be reset
   void resetGame()
   {
     //reinitialize variables to default
@@ -214,12 +204,15 @@ class gameButtonState extends State<gameButton> {
     clock.reset();
   }
 
+  //function executed when any button pressed
   void buttonPress()
   {
+    //first prevent uesr from making new moves during 250 milisecond animation
     timeOut = true;
     times.add(clock.elapsedMilliseconds);
     clock.reset();
     setState(() {
+      //check if move is "correct"- if so square should turn green and display checkmark for 100 miliseconds
       if(widget.moveCheck()) {
         consecErrors = 0;
         errors.add("correct");
@@ -229,22 +222,33 @@ class gameButtonState extends State<gameButton> {
         Timer(Duration(milliseconds: 100), () {
           if (this.mounted) {
             setState(() {
-              maze1.button_grid[widget.id].color = Colors.purple;
+              if(widget.id==99)
+                {
+                  maze1.button_grid[widget.id].color = Colors.grey;
+                }
+             else
+               {
+                 maze1.button_grid[widget.id].color = Colors.purple;
+               }
               turnGrey();
               maze1.button_grid[widget.id].displayImage = false;
               timeOut = false;
             });
           }
         });
-        //ending condition
+        //ending condition- if move is correct AND the last square on the path then game should display message congratulating them
         if(widget.id==99) {
-          Timer(Duration(milliseconds: 100), () {
-           maze1.button_grid[99].color=Colors.grey;
-          });
+
+          //when user reaches end of maze successfully all the information gathered on their run should be written to a json file
+          writeToFile("Moves", moves);
+          writeToFile("Path", path);
+          writeToFile("Times", times);
+          writeToFile("Errors", errors);
           attemptNum++;
           showDialog(
               context: context,
               builder: (BuildContext context) {
+                //return alert congratulating user if reach end of maze successfully
                 return AlertDialog(
                     title: new Text("Success!"),
                     actions: <Widget>[
@@ -259,16 +263,13 @@ class gameButtonState extends State<gameButton> {
                 );
               }
           );
-          writeToFile("Moves", moves);
-          writeToFile("Path", path);
-          writeToFile("Times", times);
-          writeToFile("Errors", errors);
 
         }
         correctMoves.add(widget.id);
       }
-      else
+      else //this code runs if move is INCORRECT- if move is incorrect then square turns red and displays an X
       {
+        //keep track of how many consecutive erros user has made- if 3 then game should show next correct move
         consecErrors++;
         errors.add("incorrect");
         maze1.button_grid[widget.id].color = Colors.red;
@@ -292,6 +293,7 @@ class gameButtonState extends State<gameButton> {
     recentMove=widget.id;
   }
   @override
+  //build actual GUI of "gamebutton" - a flatbutton with an icon hidden within it (either check or X)
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(border: Border.all(color: Colors.black)),
@@ -299,6 +301,7 @@ class gameButtonState extends State<gameButton> {
             color: maze1.button_grid[widget.id].color,
             materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
             onPressed: () {
+              //what happens on buttonPress event- either nothing (if animation is taking place) or buttonPress function (defined above) called
               timeOut?null:buttonPress();
             },
             child: maze1.button_grid[widget.id].displayImage?Column(// Replace with a Row for horizontal icon + text
@@ -310,12 +313,14 @@ class gameButtonState extends State<gameButton> {
   }
 }
 
+//function allows color of buttons to be changed by sending that gameButton a notification
 class PressNotification extends Notification {
   final int id;
   final Color color;
 
   const PressNotification({this.id, this.color});
 }
+//maze is the collection of all 100 buttons- initialized here
 class maze extends StatefulWidget {
   List<gameButton> button_grid  = [
     for (var i = 0; i < 100; i++) new gameButton(i, Colors.grey, false)
@@ -325,11 +330,12 @@ class maze extends StatefulWidget {
   mazeState createState() => mazeState(button_grid);
 
 }
-
+//state of maze
 class mazeState extends State<maze> {
   List<gameButton> button_grid;
   mazeState(this.button_grid);
 
+  //function utilizes PressNotification class and allows user to change the colors of individual squares on the maze
   bool updateButton(PressNotification notification) {
     setState(() {
       button_grid[notification.id].color = notification.color;
@@ -337,6 +343,7 @@ class mazeState extends State<maze> {
     return true;
   }
 
+ //initialize state + start clock once
   void initState()
   {
     super.initState();
@@ -344,15 +351,19 @@ class mazeState extends State<maze> {
   }
 
   @override
+  //build and return concrete implemenation of maze class
   Widget build(BuildContext context) {
+    //wrapped in notification listener so each square can listen for color change event
     return NotificationListener<PressNotification>(
       onNotification: updateButton,
       child:
       Column(
           children: <Widget>[
             GridView.builder(
+               //uniqueKey utilized so buttons that need to change color can dynamically rebuild
                 key: UniqueKey(),
                 itemCount: 100,
+                //squares kept in 10x10 gridview
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 10,
                     crossAxisSpacing: 0,
@@ -369,15 +380,15 @@ class mazeState extends State<maze> {
     );
   }
 }
-
+//homepage class initialized as class that contains everything
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
+  //state set- shouldn't need to change much since maze class is thing thats changing
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
